023309a17c47ac9b2f4b7cedae41d31564f04786
simylein <67859356+simylein@users.noreply.github.com>
2023-08-08 21:15:46 +0200

    feat(metric): add metric example

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,8 @@ import { bootstrap } from 'lib/core';
 import './auth/auth.controller';
 import './docs/docs.controller';
 import { logError, logInfo, logWarn } from './log/log.service';
+import { logRequest, logResponse } from './metric/metric.service';
 import './todo/todo.controller';
 
 const server = bootstrap();
-server.logger({ info: logInfo, warn: logWarn, error: logError });
+server.logger({ req: logRequest, res: logResponse, info: logInfo, warn: logWarn, error: logError });
