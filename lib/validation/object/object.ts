fb2d5a5e3a12d9a978b12bd983934e6dc5e1fee1
simylein <67859356+simylein@users.noreply.github.com>
2023-08-26 22:12:14 +0200

    fix(validation): object with optional default keys

--- lib/validation/object/object.ts
+++ lib/validation/object/object.ts
@@ -40,6 +40,8 @@ export const object = <T, S extends Record<string, Parser<T>>>(
 					throw keyIsMissing(key, schema[key].type[0]);
 				} else if (!schema[key].type.includes('undefined')) {
 					throw keyIsNot(key, schema[key].type, argument[key as keyof typeof argument]);
+				} else {
+					(argument as Record<string, unknown>)[key] = schema[key].parse(argument[key as keyof typeof argument]);
 				}
 			}
 		}
